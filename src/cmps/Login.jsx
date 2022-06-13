a99afa9a1bcab175a61263b56686ab34b7c6550e
Lior leizerovitch <liorle1997@gmail.com>
2022-06-13 15:05:08 +0300

    sign up without google

--- src/cmps/Login.jsx
+++ src/cmps/Login.jsx
@@ -1,35 +1,60 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/login.css'
 import { auth, provider } from '../firebase'
 import { actionTypes } from '../reducer'
 import { useStateValue } from '../Stateprovider'
 
 function Login() {
-    const [{},dispatch] = useStateValue();
-     const  signIn = async () => {
-         try{
-             const result = await auth.signInWithPopup(provider)
-             dispatch({
-                 type:actionTypes.SET_USER,
-                 user:result.user.multiFactor.user,
-             })
+    const [{ }, dispatch] = useStateValue();
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [signUp, setSignUp] = useState(true);
+    const signInWithGoogle = async () => {
+        try {
+            const result = await auth.signInWithPopup(provider)
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user.multiFactor.user,
+            })
+        }
+        catch (err) {
+            alert(err.message)
+        }
+    }
+
+    const signUpWhatsapp = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: actionTypes.SET_USER,
+            user: {
+                displayName: username,
+                password,
+                photoURL:'',
             }
-            catch(err){
-                alert(err.message)
-            }
+        })
+        setUserName('');
+        setPassword('');
     }
     return (
         <div className='login'>
-            <div className="login-container">
+            {!signUp && <div className="login-container">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2044px-WhatsApp.svg.png" alt="" />
                 <div className="login-text">
                     <h1>Sign in to whatsapp</h1>
                 </div>
-                <Button onClick={signIn}>
+                <Button onClick={signInWithGoogle}>
                     Sign in with Google
                 </Button>
-            </div>
+                <p onClick={()=>setSignUp(true)}>Dont have a google account ? Sign Up</p>
+            </div>}
+            {signUp && <form className="sign-up">
+                <h3>Sign up to whatsapp</h3>
+                <input type="text" className="username" onChange={(e) => setUserName(e.target.value)} placeholder='Enter username' />
+                <input type="password" className="password" onChange={(e) => setPassword(e.target.value)} placeholder='Enter password' />
+                <button onClick={signUpWhatsapp}>Sign Up</button>
+                <p onClick={()=>setSignUp(false)}>I want to sign in with google</p>
+            </form>}
         </div>
     )
 }
